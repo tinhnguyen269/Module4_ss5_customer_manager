7d10d1a5e966e98670667b049277095f4fdfcc53
tinhnguyen269 <tinhnguyen180507@gmail.com>
2024-07-28 08:49:25 +0700

    thuc hanh

---
+++ src/main/java/com/example/customer_manager/services/ICustomerService.java
@@ -0,0 +1,17 @@
+package com.example.customer_manager.services;
+
+import com.example.customer_manager.models.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
